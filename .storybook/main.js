7a6278be8f4bcf01a77f03eb0a79ec986bfbbf55
gouthamjm <goutham@designstring.com>
2022-07-04 16:42:48 +0530

    :fire: remove sample stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,13 +6,15 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-actions",
   ],
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-vite"
   },
   "features": {
-    "storyStoreV7": true
+    "storyStoreV7": true,
+    "interactionsDebugger": true,
   }
 }
